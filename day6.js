8a7a0764ff58a008d2fab5c97ae79ad7fcecfc33
TejalK-4954 <tejalkauthe11@gmail.com>
2025-08-13 14:56:35 +0530

    object methods

--- day6.js
+++ day6.js
@@ -35,3 +35,65 @@ console.log(delete obj.email);
 ;
 console.log(obj);
 
+//OBJECT METHODS
+//1.object.assign(target,source)
+
+let bank={};
+let bank2={
+
+    name:"idfc",
+    branch:"nashik"
+}
+
+let bank3={
+    accType:"saving",
+    balance:40000
+};
+Object.assign(bank,bank2);//assign used to copy the object into target object
+Object.assign(bank,bank3);//you can add multiple sources 
+console.log(bank);
+
+
+//2.object.create(object name)
+
+let hospital={
+    name:"civil",
+    city:"pune"
+}
+
+let hospital2 = Object.create(hospital);//create use a new object from another object
+console.log(hospital2);
+
+// hospital2.city="mumbai";
+console.log(hospital2);
+
+//3.object.freeze(object name)
+
+Object.freeze(hospital);//freeze use to make object immutable,you are not add the new property and cant change it
+
+//4.isfrozen()
+
+console.log(Object.isFrozen(hospital));//it give true or false 
+
+//5.seal()
+
+let objj={
+    id:1,
+    name:"abc"
+}
+
+objj.id=3;
+Object.seal(objj);//seal allow you to modify existing property but it wont allow you to add new property
+objj.city="pune";
+console.log(objj);
+
+//6.isSealed()
+
+console.log(Object.isSealed(objj));//return true or false
+
+
+//7.entries()
+
+console.log(Object.entries(objj));//enteries returns the key value pair of object in the format of an array
+
+
