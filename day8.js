2c9d2f780040495fd7a360f163e7766f31dfc05f
TejalK-4954 <tejalkauthe11@gmail.com>
2025-08-21 12:23:42 +0530

    destructuring programs

---
+++ day8.js
@@ -0,0 +1,18 @@
+//DESTRUCTURING
+
+let arr=['a','b','c','d','e'];
+
+let[m,n,o,p,q]=arr;//destructuring
+
+console.log(m);
+console.log(n);
+console.log(o);
+console.log("************************************************");
+
+
+let[s,,,,t]=arr;//if you want only specific elemts then skip other elements by using ( , ).
+console.log(s);
+console.log(t);
+
+
+
