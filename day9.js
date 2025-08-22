607f0ba825b5f8da5a14045b07aeb41c2721e8e9
TejalK-4954 <tejalkauthe11@gmail.com>
2025-08-22 20:01:32 +0530

    javascript string

---
+++ day9.js
@@ -0,0 +1,14 @@
+let str="city";
+
+console.log(typeof(str));
+
+console.log(str.at(2));//at  gives the  charecter at specific index
+
+console.log(str.charAt(1));//gives the  charecter at specific index
+
+console.log(str.charCodeAt(1));//charcodeAt  gives the ascii value of that index charecter
+console.log(str.charCodeAt());//if we doesnt provide any index then the it gives 0th index asciii value
+
+
+
+
