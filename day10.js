cd7c60333a44950969cb7ccbadaea69852842651
TejalK-4954 <tejalkauthe11@gmail.com>
2025-08-25 22:52:49 +0530

    Asynchronous in js

---
+++ day10.js
@@ -0,0 +1,28 @@
+//Asynchronous  programming in js
+//1.settimeout(handler,time)\
+
+// setTimeout(print,1000);
+
+function print()
+{
+    flag=true;
+    console.log("print something....");
+    
+    if(flag==true)
+{
+    clearTimeout(timer);
+}
+
+}
+
+console.log("hello");
+console.log("byeee");
+
+
+//2.clearTimeout()
+let timer=setTimeout(print,1000);
+flag=false;
+
+
+
+
