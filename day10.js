5c3b3c3a7473659ccd54245619ffa177ac613610
TejalK-4954 <tejalkauthe11@gmail.com>
2025-08-28 15:04:12 +0530

    Aynchronous methods

--- day10.js
+++ day10.js
@@ -26,4 +26,29 @@ flag=false;
 //clear timeout clear settimeout
 
 
+//3.setInterval
 
+function greet()
+{
+    console.log("great day!!");
+    
+}
+setInterval(greet,1000);//after that specific time the greet function is called
+
+
+//4.clearInterval
+let count=1;
+
+function demo()
+{
+    console.log("hello from demo");
+    count++;
+
+    if(count>5)
+    {
+        clearInterval(interval1);
+    }
+    
+}
+
+let interval1=setInterval(demo,2000);
