b6261bffde44f25b90259ac9abe82850bea18573
TejalK-4954 <tejalkauthe11@gmail.com>
2025-09-04 13:09:41 +0530

    lexical scope

---
+++ day14.js
@@ -0,0 +1,40 @@
+//lexical Scope/closure
+
+function account()
+{
+   let bal=10000;
+
+   function deposit(v)
+   {
+       bal=bal+v;
+       console.log("updated balance is:"+bal);
+       
+   }
+
+   function withdraw(v)
+   {
+
+    if(bal<v)
+    {
+        console.log("insufficient balance");  
+    }
+    else{
+     bal=bal-v;
+     console.log("withdraw amount:"+v);
+     console.log("balance after withdraw:"+bal);
+    }
+   }
+
+   function returnbalance()
+   {
+    console.log("balance::"+bal);
+    ;
+   }
+
+   return [deposit,withdraw,returnbalance];
+}
+
+let [deposit,withdraw,returnbalance]=account();
+deposit(4000);
+withdraw(2000);
+returnbalance();
