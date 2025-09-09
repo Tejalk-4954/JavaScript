0c40e6d04b1c74bd267bca1789e9ec567b394759
TejalK-4954 <tejalkauthe11@gmail.com>
2025-09-09 19:43:46 +0530

    protypal Inheritance

---
+++ day18.js
@@ -0,0 +1,20 @@
+let person1={
+    names:"tejal",
+    city:"nashik",
+    age:21
+}
+let person2={
+
+    names:"kartiki",
+    city:"pune",
+    age:22
+}
+
+Object.prototype.demo=function()
+{
+    console.log("hii");
+   
+}
+
+person1.demo();
+person2.demo();
